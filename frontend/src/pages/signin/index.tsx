d751691fb80ce8fbc6fd2487b488eccc36442481
Matheus Silva <matheus_lsilva@hotmail.com>
2022-12-01 13:02:39 -0300

    feat: signin page

---
+++ frontend/src/pages/signin/index.tsx
@@ -0,0 +1,70 @@
+import seaOfClouds from 'assets/seaOfClouds.svg';
+import SignInContainer from './style';
+
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsKey, BsArrowRightShort } from 'react-icons/bs';
+
+import Input from 'components/input';
+import Button from 'components/button';
+
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { ILoginRequest } from 'interfaces/sessions/index';
+import { Span } from '../../components/span/style';
+import { WavyLink } from 'react-wavy-transitions';
+import SessionContainer from 'components/sessionContainer';
+import signinSchema from 'schemas/signup';
+
+const SignInPage = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<ILoginRequest>({
+        resolver: zodResolver(signinSchema),
+    });
+
+    const handleForm = (data: ILoginRequest) => {
+        console.log(data);
+    };
+
+    return (
+        <SignInContainer className="md-box">
+            <SessionContainer>
+                <form onSubmit={handleSubmit(handleForm)} className="form">
+                    <h2>Login</h2>
+                    <h3>Vamos juntos nessa!</h3>
+                    <Input
+                        Icon={HiOutlineMail}
+                        placeholder="Email"
+                        type="text"
+                        register={register}
+                        name="email"
+                    />
+                    <Span>{errors.email?.message}</Span>
+
+                    <Input
+                        Icon={BsKey}
+                        placeholder="Senha"
+                        type="password"
+                        register={register}
+                        name="password"
+                    />
+                    <Span>{errors.password?.message}</Span>
+                    <p>
+                        Ainda não se cadastrou?
+                        <span>
+                            <WavyLink to="/" color="#216ce7">
+                                Cadastre-se aqui!
+                            </WavyLink>
+                        </span>
+                    </p>
+                    <Button content="Cadastrar" Icon={BsArrowRightShort} />
+                </form>
+            </SessionContainer>
+            <img className="clouds" src={seaOfClouds} alt="Cloud" />
+        </SignInContainer>
+    );
+};
+
+export default SignInPage;
---
+++ frontend/src/pages/signin/style.ts
@@ -0,0 +1,42 @@
+import styled, { keyframes } from 'styled-components';
+
+const clouds = keyframes`
+    100% {
+        translate: -90vw -55%;
+        scale: 2 1.1;
+    }
+`;
+
+const SignInContainer = styled.div`
+    background: #3284ce;
+    font-family: 'Euclid Circular A';
+
+    display: flex;
+    flex-direction: row-reverse;
+
+    .clouds {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 2000px;
+        height: 2500px;
+        translate: 0% -35%;
+        animation: ${clouds} 25s infinite linear;
+    }
+
+    main {
+        position: static;
+        height: 100vh;
+
+        .form {
+            gap: 25px;
+        }
+    }
+
+    @media (max-width: 500px) {
+        .clouds {
+            display: none;
+        }
+    }
+`;
+export default SignInContainer;
